f50ddb7e508ae8a7447d5a0156998d0f2bff31af
Isla4ever <70209532+Isla4ever@users.noreply.github.com>
2020-09-26 21:37:08 +0800

    Add Group and Direct Message Support

--- src/main/java/dev/cardinal/listener/MessageListener.java
+++ src/main/java/dev/cardinal/listener/MessageListener.java
@@ -6,18 +6,42 @@ import dev.cardinal.utils.MessageUtils;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.ListeningStatus;
 import net.mamoe.mirai.event.SimpleListenerHost;
-import net.mamoe.mirai.message.MessageEvent;
+import net.mamoe.mirai.message.FriendMessageEvent;
+import net.mamoe.mirai.message.GroupMessageEvent;
+import net.mamoe.mirai.message.TempMessageEvent;
 
 public class MessageListener extends SimpleListenerHost {
     @EventHandler
-    public ListeningStatus onMessgae(MessageEvent event) {
+    public ListeningStatus onTempMessgae(TempMessageEvent event) {
         String message = MessageUtils.messageToString(event.getMessage());
 
         if (ConfigData.pterodactylEnabled) {
             pterodactyl pterodactyl = new pterodactyl();
-            pterodactyl.onMessage(message,event.getSender());
+            pterodactyl.onDirectMessage(message,event.getSender());
         }
+        return ListeningStatus.LISTENING;
+    }
 
+    //This should be the same as onTempMessage
+    @EventHandler
+    public ListeningStatus onFriendMessage(FriendMessageEvent event) {
+        String message = MessageUtils.messageToString(event.getMessage());
+
+        if (ConfigData.pterodactylEnabled) {
+            pterodactyl pterodactyl = new pterodactyl();
+            pterodactyl.onDirectMessage(message,event.getSender());
+        }
+        return ListeningStatus.LISTENING;
+    }
+
+    @EventHandler
+    public ListeningStatus onGroupMessage(GroupMessageEvent event) {
+        String message = MessageUtils.messageToString(event.getMessage());
+
+        if (ConfigData.pterodactylEnabled) {
+            pterodactyl pterodactyl = new pterodactyl();
+            pterodactyl.onGroupMessage(message,event.getGroup(),event.getSender());
+        }
         return ListeningStatus.LISTENING;
     }
 }
